3abfda2b7c209ea55c6c3c3e939c3e4197f940ec
Mats Estensen <matsest@mxe.no>
2024-12-02 12:01:48 +0100

    chore: remove dupe tests

--- 02/main.Tests.ps1
+++ 02/main.Tests.ps1
@@ -26,21 +26,6 @@ Describe 'Part 1' {
     }
 }
 
-Describe 'Test-Report' {
-    It 'decreasing by two is safe' {
-        Test-Report @(7, 6, 4, 2, 1) | Should -Be $True
-    }
-    It 'increasing by five is unsafe' {
-        Test-Report @(9, 7, 6, 2, 1) | Should -Be $False
-    }
-    It 'mixed increasing/decreasing is unsafe' {
-        Test-Report @(1, 3, 2, 4, 5) | Should -Be $False
-    }
-    It 'no increase/decrease unsafe' {
-        Test-Report @(8, 6, 4, 4, 1) | Should -Be $False
-    }
-}
-
 Describe 'Part 2' {
     It 'matches expected output (4)' {
         Invoke-Part2 -Path "test.txt" | Should -Be 4
